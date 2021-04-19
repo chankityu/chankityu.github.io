6703c1d11c51890bb35489a3131f3d440fcef4fd
chankityu <yuchankit@gmail.com>
2021-04-19 16:31:57 +0800

    Update functionize-collector.js

--- functionize-collector.js
+++ functionize-collector.js
@@ -4854,7 +4854,7 @@ if (typeof window.functionizePluginInstalled == "undefined" || !window.functioni
                     return false;
                 }
                 // TODO: Fix this.
-                var blacklist = ['META', 'HEAD', undefined ];
+                var blacklist = ['META', 'HEAD', 'TITLE', 'LINK', 'STYLE', 'NOSCRIPT', 'BODY', undefined ];
                 if (!blacklist.includes(element.nodeName)){
                     element.textContent = piiFilter.filterPII(element.textContent);
                 }
