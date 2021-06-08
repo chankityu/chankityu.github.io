37a593606ace6fceb889f5bfabefa7d27395615d
chankityu <yuchankit@gmail.com>
2021-06-08 17:36:27 +0800

    Update functionize-collector.js

--- functionize-collector.js
+++ functionize-collector.js
@@ -4523,7 +4523,7 @@ if (typeof window.functionizePluginInstalled == "undefined" || !window.functioni
             .run()
             .then(results => {
                 var sentJson, pass, image;
-                var elementStatistics = WS.siteStastistics.getSelection2(document.body);
+                var elementStatistics = WS.siteStatistics.getSelection2(document.body);
                 console.log(elementStatistics);
                 if (results.violations.length) {
                     for(var i=0; i < results.violations[0].nodes.length; i++) {
