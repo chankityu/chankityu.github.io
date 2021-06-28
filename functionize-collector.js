09a0b9113d7e461c5d85a48e99b9c027537cc01e
chankityu <yuchankit@gmail.com>
2021-06-28 12:43:00 +0800

    Update functionize-collector.js

--- functionize-collector.js
+++ functionize-collector.js
@@ -4670,10 +4670,10 @@ if (typeof window.functionizePluginInstalled == "undefined" || !window.functioni
               } else if (nt === 3) {
                   if(piiFilter.filterPII(node.data) !== node.data) {
                       console.log("Node data is " + node.data);
-                      //console.log(node.getBoundingClientRect());
-                      const rects = range.getClientRects();
-                      range.selectNodeContents(node);
-                      console.log(rects);
+                      console.log(node.getBoundingClientRect());
+                    //   const rects = range.getClientRects();
+                    //   range.selectNodeContents(node);
+                    //   console.log(rects);
                   }
                 range.selectNodeContents(node);
                 const rects = range.getClientRects();
